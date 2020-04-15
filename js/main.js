26b6dd7c91186b551a779e8d0e1294f32cee7c95
Tubasa Matsuoka <workwing0283@gamil.com>
2020-04-15 15:25:02 +0900

    短い文章で表現

--- js/main.js
+++ js/main.js
@@ -3,7 +3,21 @@
 {
   const btn =document.getElementById('btn');
 
-  btn.addEventListener('click',() =>{
-    btn.textContent = 'hit!';
+  btn.addEventListener('click',() => {
+    const results = ['大吉','中吉','凶','末吉'];
+    // const n = Math.floor(Math.random() * result.length);
+    // btn.textContent = result[n];
+    btn.textContent = results[Math.floor(Math.random() * results.length)];
+    // switch(n){
+    //   case 0:
+    //   btn.textContent ='大吉';
+    //   break;
+    //   case 1:
+    //   btn.textContent ='中吉';
+    //   break;
+    //   case 2:
+    //   btn.textContent ='凶';
+    //   break;
+    // }
   });
 }
